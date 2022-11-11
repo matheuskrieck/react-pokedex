19bf072d62a1473542b4e9519e5e5c4fe42022fe
matheuskrieck <110129434+matheuskrieck@users.noreply.github.com>
2022-11-11 20:06:26 -0300

    minor adjustments

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,7 +5,7 @@ const Searchbar = (props) => {
   const { onSearch } = props;
 
   const onChangeHandler = (e) => {
-    setSearch(e.target.value);
+    setSearch(e.target.value.toLowerCase());
     if (e.target.value.length === 0) {
       onSearch(undefined);
     }
